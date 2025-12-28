16508942d7750a3fa3593bb2b7bca647cc53046e
chengjiafeng857 <feng.chen@northeastern.edu>
2025-12-28 05:45:16 -0500

    update layout

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8000",
+        target: "http://157.151.227.133:8000",
         changeOrigin: true,
       },
     },
